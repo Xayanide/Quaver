8f419a12d98dfe7b4ed66788d7b4b133c1fb8d0f
Xayanide <chris.balili.91@gmail.com>
2022-07-20 11:05:34 +0800

    feat(nix): update to nodejs v18

--- replit.nix
+++ replit.nix
@@ -1,5 +1,22 @@
-{ pkgs }: {
-	deps = with pkgs; [
-		nodejs-16_x
-	];
+# { pkgs }: {
+#	deps = with pkgs; [
+#		nodejs-16_x
+#	];
+# }
+{ pkgs }: let
+    nixpkgs = builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/nixos-22.05.tar.gz";
+        # sha256 = "18qhrp5jbn51vgc534xlyz1hjjsv5syxh5mshwgkyqh5k3njdzlh";
+    };
+
+    pkgs = import nixpkgs {
+        config = { 
+            allowUnfree = true;
+        }; 
+    };
+
+in {
+    deps = [
+        pkgs.nodejs-18_x
+    ];
 }
